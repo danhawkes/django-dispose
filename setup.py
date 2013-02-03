8f3274b72ad5ac0227b96464ad51a4034e4b6793
danhawkes <dan@danhawkes.co.uk>
2013-02-03 17:29:19 +0000

    Increments version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name='django-dispose',
     description='Disposes of media files that are no longer referenced in the database.',
-    version='1.0',
+    version='1.1',
     author='Daniel Hawkes',
     author_email='dan@danhawkes.co.uk',
     license='MIT',
